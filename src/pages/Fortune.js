b4001f6fbd765312730eabe51f6145c8eea95905
JenniferTHSQ <jennifer24324@gmail.com>
2025-03-26 01:24:39 +0800

    改token代號

--- src/pages/Fortune.js
+++ src/pages/Fortune.js
@@ -32,7 +32,7 @@ export default function Fortune() {
       const response = await fetch("https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta", {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${process.env.REACT_APP_HF_API_TOKEN}`,
+          Authorization: `Bearer ${process.env.REACT_APP_HF_TOKEN}`,
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
